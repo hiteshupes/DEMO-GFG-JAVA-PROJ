b5035d2cd3e6f36438e642d49bc2769bdac452ba
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-02-23 17:42:56 +0530

    sum() added

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,15 @@
+package mypkg;
+
+public class MyCalc {
+	
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is : "+calc.sum(20, 10));
+	}
+}
